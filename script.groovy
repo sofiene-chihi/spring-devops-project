1b84689585980c9cf901ac51720135cf39eff90e
sofiene chihi <sofienechihi@insat.u-carthage.tn>
2022-10-11 14:36:29 +0100

    fix: updated dev server user

--- script.groovy
+++ script.groovy
@@ -16,14 +16,14 @@ def deployApp() {
     echo 'deploying the application...'
     def composeRun = '"export MYSQLDB_USER=root MYSQLDB_ROOT_PASSWORD=sofiene MYSQLDB_DATABASE=digihunt MYSQLDB_LOCAL_PORT=3306 MYSQLDB_DOCKER_PORT=3306 SPRING_LOCAL_PORT=8081 SPRING_DOCKER_PORT=8080 && docker-compose -f digi-back.yaml up -d "'
     sshagent (credentials: ['dev-server']) {
-        sh "ssh -o StrictHostKeyChecking=no sofiene@194.195.246.28 ${composeRun}"
+        sh "ssh -o StrictHostKeyChecking=no root@194.195.246.28 ${composeRun}"
     }
 }
 
 def cleanUntaggedImages(){
     def cleanImages = 'docker image prune --force --filter "dangling=true"'
     sshagent (credentials: ['dev-server']) {
-        sh "ssh -o StrictHostKeyChecking=no sofiene@194.195.246.28 ${cleanImages}"
+        sh "ssh -o StrictHostKeyChecking=no root@194.195.246.28 ${cleanImages}"
     }
 }
 
