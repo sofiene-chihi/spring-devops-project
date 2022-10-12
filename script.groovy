eb4771315649491e7c6de30c38125255a0e65612
sofiene chihi <sofienechihi@insat.u-carthage.tn>
2022-10-12 10:47:47 +0100

    fix: updated docker compose for server deployment

--- script.groovy
+++ script.groovy
@@ -14,7 +14,7 @@ def testApp() {
 
 def deployApp() {
     echo 'deploying the application...'
-    def composeRun = '"export MYSQLDB_USER=root MYSQLDB_ROOT_PASSWORD=sofiene MYSQLDB_DATABASE=digihunt MYSQLDB_LOCAL_PORT=3306 MYSQLDB_DOCKER_PORT=3306 SPRING_LOCAL_PORT=8081 SPRING_DOCKER_PORT=8080 && docker-compose -f digi-back.yaml up -d "'
+    def composeRun = '"docker-compose up -d"'
     sshagent (credentials: ['dev-server']) {
         sh "ssh -o StrictHostKeyChecking=no root@194.195.246.28 ${composeRun}"
     }
