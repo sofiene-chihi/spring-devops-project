589ca23b4bd71ea54ddd26037e64054c7295fb90
sofiene chihi <sofiene@pop-os.localdomain>
2022-10-22 05:02:24 +0100

    test: testing jacoco test coverage with sonarqube

--- src/main/java/com/example/devopsproject/DevopsProjectApplication.java
+++ src/main/java/com/example/devopsproject/DevopsProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DevopsProjectApplication.class, args);
     }
