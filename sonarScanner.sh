7aab7b11a3669f688413b2f55e31019f7e40ec15
sofiene chihi <sofiene@pop-os.localdomain>
2022-11-06 02:16:59 +0100

    updated sonarScanner script

--- sonarScanner.sh
+++ sonarScanner.sh
@@ -1,4 +1,4 @@
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=spring-project \
-  -Dsonar.host.url=http://194.195.246.28:9000 \
-  -Dsonar.login=sqp_57a74eb0c4b000f10192817b1acd8bafd4aabb7b
+  -Dsonar.host.url=http://192.168.122.144:9000 \
+  -Dsonar.login=sqp_57aeff21d5ada3916b2d5748e6ca4071bf319570
