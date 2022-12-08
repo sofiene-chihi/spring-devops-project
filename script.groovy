91c558f6b5241a349180aa6bdd25b26b1ec1efd9
sofiene chihi <chihisofiene2@gmail.com>
2022-12-08 15:27:11 +0100

    feat: incrementing docker image version with each commit

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t sofienechihi/my-repo:spring-app-1.0 .'
+        sh "docker build -t sofienechihi/my-repo:spring-app-${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push sofienechihi/my-repo:spring-app-1.0'
     }
